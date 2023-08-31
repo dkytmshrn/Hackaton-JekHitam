6808244b044a05c7a922b7660f6b93474ee40b50
dkytmshrn <dkytmshrn@gmail.com>
2023-09-01 04:54:25 +0700

    tama

--- tebak-kartu.js
+++ tebak-kartu.js
@@ -1,3 +1,4 @@
+
 let deck
 let cardBefore = 0
 let cardNow = 0
@@ -9,13 +10,14 @@ window.onload = function () {
     buildDeck();
     shuffleDeck();
     startGame();
-    var name = prompt("Masukan nama kamu", "");
+    var name = prompt("AHOOY PIRATE!! AYO TEBAK NILAI KARTU SELANJUTNYA YANG AKAN KELUAR!!! SIAPA NAMA MU?!!", "");
     if (name != null && name != "") {
-      playerName = name;
+        playerName = name;
     }
     document.getElementById("username").innerHTML += playerName
 }
 
+console.log(webDefault);
 function userName(name) {
     let result = {
         name: name,
@@ -28,13 +30,13 @@ function buildDeck() {
     let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
     let types = ["C", "D", "H", "S"];
     deck = [];
-
+    
     for (let i = 0; i < types.length; i++) {
         for (let j = 0; j < values.length; j++) {
             deck.push(values[j] + "-" + types[i])
         }
     }
-    // console.log(deck);
+    console.log(deck);
 }
 
 function shuffleDeck() {
@@ -48,38 +50,27 @@ function shuffleDeck() {
 }
 
 function startGame() {
-
+    
     let cardImage = document.createElement("img")
     let cardRandom = deck.pop()
     cardImage.src = "./images/" + cardRandom + ".png"
     cardNow = getValue(cardRandom)
     cardBefore = cardNow
     document.getElementById("your-cards").append(cardImage)
-
+    
     document.getElementById("up").addEventListener("click", up)
     document.getElementById("down").addEventListener("click", down)
 }
 
-function resetGame() {
-    // Diclear keadaan gamenya, hilang kartu dan score
-}
-
 function reset() {
-    document.getElementById("reset").addEventListener("click", function () {
-        resetGame();
-        buildDeck();
-        shuffleDeck();
-        startGame();
-        console.log('RESET');
-    }
-    )
+    location.reload()
 }
 
 
 function getValue(card) {
     let data = card.split("-")
     let value = data[0];
-
+    
     if (value === "A") {
         return 14;
     }
@@ -99,7 +90,7 @@ function up() {
     if (!button) {
         return
     }
-
+    
     let cardImg = document.createElement("img")
     let cardRandom = deck.pop()
     cardImg.src = "./images/" + cardRandom + ".png"
@@ -108,7 +99,7 @@ function up() {
     x.append(cardImg.cloneNode(true))
     let y = document.getElementById("randcard")
     y.src = "./images/" + cardRandom + ".png"
-
+    
     if (cardNow > cardBefore) {
         totalPoint += 1000
         document.getElementById("score").innerHTML = `Score: ${totalPoint}`
@@ -116,25 +107,27 @@ function up() {
     else {
         live--
     }
-
+    
     if (live === 0) {
         button = false
-        message = `KAMU KALAH`
+        message = `WALK ON PLANK YOU LOSER!!`
         document.getElementById("results").innerText = message;
         document.getElementById("reset").style.display = 'block'
-        document.getElementById("buttons").style.display = 'none'
+        document.getElementById("up").style.display = 'none'
+        document.getElementById("down").style.display = 'none'
     }
-
+    
     cardBefore = cardNow
-
+    
     if (totalPoint === 5000) {
-        message = `SELAMAT KAMU MENANG`
+        message = `YOU GOT ME!!`
         document.getElementById("results").innerText = message;
         document.getElementById("score").innerHTML = `Score: ${totalPoint}`
         document.getElementById("reset").style.display = 'block'
-        document.getElementById("buttons").style.display = 'none'
+        document.getElementById("up").style.display = 'none'
+        document.getElementById("down").style.display = 'none'
     }
-    reset()
+    document.getElementById("reset").addEventListener("click", reset)
 }
 
 function down() {
@@ -142,7 +135,7 @@ function down() {
         return
     }
     let message = ''
-
+    
     let cardImg = document.createElement("img")
     let cardRandom = deck.pop()
     cardImg.src = "./images/" + cardRandom + ".png"
@@ -159,23 +152,26 @@ function down() {
     else {
         live--
     }
-
+    
     if (live === 0) {
         button = false
-        message = `KAMU KALAH`
+        message = `WALK ON PLANK YOU LOSER!!`
         document.getElementById("results").innerText = message;
         document.getElementById("reset").style.display = 'block'
         document.getElementById("buttons").style.display = 'none'
     }
-
+    
     cardBefore = cardNow
-
+    
     if (totalPoint === 5000) {
-        message = `SELAMAT KAMU MENANG`
+        message = `YOU GOT ME!!`
         document.getElementById("results").innerText = message;
         document.getElementById("score").innerHTML = `Score: ${totalPoint}`
         document.getElementById("reset").style.display = 'block'
         document.getElementById("buttons").style.display = 'none'
     }
-    reset()
+    
+    document.getElementById("reset").addEventListener("click", reset)
 }
+
+// import {webDefault} from "./body.js"
